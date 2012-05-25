4c79a45ca65e4429ca687ec87042dc1f673fce3a
Prasanth Guruprasad <gprasant@thoughtworks.com>
2012-05-25 20:32:10 +0530

    Prasanth | Added some utility methods, gitignore

---
+++ artist.py
@@ -0,0 +1,7 @@
+class Artist:
+	def __init__(self,id,name):
+		self.id = id
+		self.name = name
+        
+    def alias():
+        return self.name.lower().replace(' ', '_')
--- snoop.py
+++ snoop.py
@@ -4,6 +4,7 @@ import httplib
 import json
 import musixmatch
 from musixmatch import *
+from artist import * 
 
 def main():
     stop_file = open('stopwords.txt')
@@ -24,12 +25,12 @@ def main():
     for key in sorted(wordCount, key=wordCount.__getitem__, reverse=True):
         print key, " : ",  wordCount[key]
    
-def parse_musix_url():
+def parse_musix_url(artistId):
 #snoop dogg - 106
 #bob marley - 140038
 #50 cent - 8347
     apikey = 'aea7c70a32c3b8d56a07c99ef9d56ffa'
-    artists = musixmatch.ws.artist.albums.get(artist_id=8347, apikey=apikey)
+    artists = musixmatch.ws.artist.albums.get(artist_id=artistId, apikey=apikey)
     albums = []
     for album in artists["body"]["album_list"]:
         albums.append(album["album"]["album_id"])
@@ -45,12 +46,24 @@ def parse_musix_url():
     for trackid in tracks:
         lyric = musixmatch.ws.track.lyrics.get(track_id = trackid, apikey=apikey)
         if lyric["header"]["status_code"] == 200:
-            print lyric["body"]["lyrics"]["lyrics_body"]
-        # lyrics.append(lyric["lyrics_body"])   
-    print lyrics
+            # print lyric["body"]["lyrics"]["lyrics_body"]
+            lyrics.append(lyric["body"]["lyrics"]["lyrics_body"])   
+    return lyrics
+
+def getLyricsForArtist(artist):
+    lyrics = parse_musix_url(artist.id)
+    return lyrics
+
+def getArtists():
+    artist_file = open(r"C:\Users\Prasanth Guruprasad\code\lyrics\artist_info.txt", 'r')
+    artists = []
+    for info in artist_file.readlines():
+        artists.append( Artist(int(info.split(':')[0]), info.split(':')[1]))
+    return artists
     
 if __name__ == "__main__":
-    main()
+    print getLyricsForArtist(getArtists()[7])
+    # for artist in getArtists():
+        # getLyricsForArtist(artist)
     
-    
-  
+    #main()
